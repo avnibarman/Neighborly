b713509d7cb5308d6cf95a34669e90979befcad0
Daniyal Shafi <dshafi@usc.edu>
2018-04-08 18:47:16 -0700

    modified server.java

--- NeighborlyServer/src/Server.java
+++ NeighborlyServer/src/Server.java
@@ -1,4 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URI;
+import java.util.Set;
+
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.DeploymentException;
+import javax.websocket.Endpoint;
+import javax.websocket.Extension;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
 
 public class Server {
-
+	
+	ServerSocket ss = null;
+	
+	Server(int port){
+		try {
+			ss = new ServerSocket(port);
+			while(true)
+			{
+				System.out.println("Waiting for connection...");
+				Session session = new
+			}
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("ioe in Server constructor: " + ioe.getMessage());
+		}
+	}
 }
--- neighborly.sql
+++ neighborly.sql
@@ -5,39 +5,29 @@ USE Neighborly;
 
 CREATE TABLE User (
 	userID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fname VARCHAR(50) NOT NULL,
-    lname VARCHAR(50) NULL,
-    email VARCHAR(100) NULL,
-    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
-    image VARCHAR(300) NOT NULL,
-    address VARCHAR(300) NULL,
-    lendingCredits INT(11)
-	
+    image VARCHAR(300) NULL
 );
 
 CREATE TABLE Item (
 	itemID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    posterID INT(11), -- foreign key from user table
-    borrowerID INT(11), -- foreign key from user table
-    status INT(1), -- 0 its available, 1 it's not
+    ownerID INT(11) NOT NULL,-- foreign key from user table
+    borrowerID INT(11), -- foreign key from user table, if its null, means that it is available
+    availibility INT(1) NOT NULL,-- 0 its available, 1 it's not
     image VARCHAR(300),
     description VARCHAR(500),
-    categories VARCHAR(300), -- concatenated string of categories, split by comma
-    datesUnavailable VARCHAR(300), -- concatenated string of categories, split by comma
-    communityID INT(11) -- initialized to 0 if no group, foreign key
+    categories VARCHAR(300),
+    FOREIGN KEY fk1(ownerID) REFERENCES User(userID),
+    FOREIGN KEY fk1(borrowerID) REFERENCES User(userID)
 );
 
-CREATE TABLE CommunityGroup (
-	communityID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    image VARCHAR(300) NOT NULL,
-    name VARCHAR(50),
-    inviteCode VARCHAR(50) -- NULL if there is none 
+CREATE TABLE Categories(
+	categoryID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    category VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE UsersInCommunityGroup (
-	userID INT(11), -- foreign key
-    communityID INT(11) -- foreign key
-);
+
 
     
