f9a66bc5613360426234650607bc44c4fa92c6ca
Daniyal Shafi <dshafi@usc.edu>
2018-04-11 15:47:21 -0700

    added User Class, Item Class, Database Class (with login, signup and search by search term and distance functionality

---
+++ NeighborlyServer_Web/src/Message.java
@@ -0,0 +1,20 @@
+
+public class Message {
+	String mesage;
+	int id;
+	
+	public String getMesage() {
+		return mesage;
+	}
+	public void setMesage(String mesage) {
+		this.mesage = mesage;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
