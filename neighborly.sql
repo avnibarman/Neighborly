ae0ec9042dfb8ef3ad9ec65d487918f6aa7f2c32
Daniyal Shafi <dshafi@usc.edu>
2018-04-11 15:42:05 -0700

    fixed database file

--- neighborly.sql
+++ neighborly.sql
@@ -3,41 +3,32 @@ DROP DATABASE IF EXISTS Neighborly; /*THIS IS THE MOST DANGEROUS LINE OF SQL COD
 CREATE DATABASE Neighborly; 
 USE Neighborly;
 
-CREATE TABLE User (
+CREATE TABLE Users (
 	userID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    fname VARCHAR(50) NOT NULL,
-    lname VARCHAR(50) NULL,
-    email VARCHAR(100) NULL,
-    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    image VARCHAR(300) NOT NULL,
-    address VARCHAR(300) NULL,
-    lendingCredits INT(11)
-	
+    imageURL VARCHAR(300) NULL
 );
 
-CREATE TABLE Item (
+CREATE TABLE Items (	
 	itemID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    posterID INT(11), -- foreign key from user table
-    borrowerID INT(11), -- foreign key from user table
-    status INT(1), -- 0 its available, 1 it's not
-    image VARCHAR(300),
+    itemName VARCHAR(30) NOT NULL,
+    ownerID INT(11) NOT NULL,-- foreign key from user table
+    borrowerID INT(11) , -- foreign key from user table, if its null, means that it is available
+    availibility INT(1) NOT NULL,-- 0 its available, 1 it's not
+    imageURL VARCHAR(300),
     description VARCHAR(500),
-    categories VARCHAR(300), -- concatenated string of categories, split by comma
-    datesUnavailable VARCHAR(300), -- concatenated string of categories, split by comma
-    communityID INT(11) -- initialized to 0 if no group, foreign key
+    categories VARCHAR(300),
+	latitude DOUBLE(20,16) NOT NULL,
+    longitude DOUBLE(20,16) NOT NULL,
+    FOREIGN KEY fk1(ownerID) REFERENCES Users(userID),
+    FOREIGN KEY fk2(borrowerID) REFERENCES Users(userID)
 );
 
-CREATE TABLE CommunityGroup (
-	communityID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    image VARCHAR(300) NOT NULL,
-    name VARCHAR(50),
-    inviteCode VARCHAR(50) -- NULL if there is none 
-);
-
-CREATE TABLE UsersInCommunityGroup (
-	userID INT(11), -- foreign key
-    communityID INT(11) -- foreign key
+CREATE TABLE Categories(
+	categoryID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    category VARCHAR(20) NOT NULL
 );
 
     
