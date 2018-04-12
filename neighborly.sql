bc6724e26bea9e7aa9a2ccbb58ccbf6223c75253
Daniyal Shafi <dshafi@usc.edu>
2018-04-11 17:54:23 -0700

    nothing changed in here

--- neighborly.sql
+++ neighborly.sql
@@ -8,7 +8,7 @@ CREATE TABLE Users (
     email VARCHAR(100) UNIQUE NOT NULL,
     name VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    image BLOB
+    image MEDIUMBLOB
 );
 
 CREATE TABLE Items (	
